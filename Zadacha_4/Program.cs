979d501bde1782c52bb530ee7631e5dfb1dcdb16
Ivan <test@mail.ru>
2023-06-15 23:31:35 +0300

    Добавил решение к 4 задаче

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a > b) & (a > c))
+    Console.WriteLine($"{a}, {b}, {c}  -  большее {a} ");
+else if (b > c)
+    Console.WriteLine($"{a}, {b}, {c} - большее {b} ");
+
+else 
+    Console.WriteLine($"{a}, {b}, {c} - большее {c} ");
+
+
+
+
+ 
+
