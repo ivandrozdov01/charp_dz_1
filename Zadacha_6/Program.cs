f1f67a110c86775cc9dcc894453e2f56144983f3
Ivan <test@mail.ru>
2023-06-15 23:40:49 +0300

    Написал программу для решения 6 задачи

---
+++ Zadacha_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+    Console.WriteLine($"{n} - число является чётным, т.к. оно делится на 2 без остатка ");
+else
+    Console.WriteLine($"{n} - число является нечётным, т.к. оно делится на 2 с остатком ");
